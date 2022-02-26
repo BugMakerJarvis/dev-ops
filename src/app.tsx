30618d45f6d5db8be941838d055c879d281bbc71
Jarvis <n11152133@qut.edu.au>
2022-02-26 11:02:23 +0800

    change user login

--- src/app.tsx
+++ src/app.tsx
@@ -5,7 +5,7 @@ import type {RunTimeLayoutConfig} from 'umi';
 import {history} from 'umi';
 import RightContent from '@/components/RightContent';
 import Footer from '@/components/Footer';
-// import {currentUser as queryCurrentUser} from './services/ant-design-pro/api';
+import {currentUser as queryCurrentUser} from './services/ant-design-pro/api';
 // import { BookOutlined, LinkOutlined } from '@ant-design/icons';
 import defaultSettings from '../config/defaultSettings';
 // import {Button} from "antd";
@@ -29,25 +29,25 @@ export async function getInitialState(): Promise<{
 }> {
   const fetchUserInfo = async () => {
     try {
-      // const currentUser = await queryCurrentUser();
-      const currentUser = {
-        access: "admin",
-        address: "翻斗大街翻斗花园二号楼1001室",
-        avatar: "https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png",
-        country: "China",
-        email: "xxxxx@gmail.com",
-        geographic: {province: {label: "浙江省", key: "330000"}, city: {label: "杭州市", key: "330100"}},
-        group: "系统运维负责人",
-        name: "胡图图",
-        notifyCount: 12,
-        phone: "0752-268888888",
-        signature: "海纳百川，有容乃大",
-        tags: [{key: "0", label: "很有想法的"}, {key: "1", label: "专注设计"}, {key: "2", label: "辣~"}],
-        title: "运维",
-        unreadCount: 11,
-        userid: "00000001"
-      }
-      return currentUser;
+      const currentUser = await queryCurrentUser();
+      // const currentUser = {
+      //   access: "admin",
+      //   address: "翻斗大街翻斗花园二号楼1001室",
+      //   avatar: "https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png",
+      //   country: "China",
+      //   email: "xxxxx@gmail.com",
+      //   geographic: {province: {label: "浙江省", key: "330000"}, city: {label: "杭州市", key: "330100"}},
+      //   group: "系统运维负责人",
+      //   name: "胡图图",
+      //   notifyCount: 12,
+      //   phone: "0752-268888888",
+      //   signature: "海纳百川，有容乃大",
+      //   tags: [{key: "0", label: "很有想法的"}, {key: "1", label: "专注设计"}, {key: "2", label: "辣~"}],
+      //   title: "运维",
+      //   unreadCount: 11,
+      //   userid: "00000001"
+      // }
+      return currentUser.data;
     } catch (error) {
       history.push(loginPath);
     }
